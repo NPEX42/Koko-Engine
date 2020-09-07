5aaaadb614050b8dc4e9394cd6191f2bbc03dc07
Npex <gvenn666@gmail.com>
2020-09-07 16:03:25 +0100

    Added Events Docs :100:

---
+++ Koko-Engine/src/koko/events/EventSystem.java
@@ -0,0 +1,11 @@
+package koko.events;
+
+public class EventSystem {
+	public static void DispatchCloseEvent() {
+		
+	}
+	
+	public static void glfw_DispatchCloseEvent() {
+		DispatchCloseEvent();
+	}
+}
